7040ace0851d77d1441c5081abb86662f422ed48
Dmitry <88_d1m@mail.ru>
2023-10-13 20:12:32 +0300

    Завершил написание программы, проверил на наличие ошибок

--- Сontrol_work.java
+++ Сontrol_work.java
@@ -4,8 +4,29 @@ import java.util.Arrays;
 public class Сontrol_work {
     public static void main(String[] args) {
         String str = "Hello, 2, world, :-)";
+        getNewString(str);
+        
     }
+    public static void getNewString(String str) {
+        String cleanString = str.replace(",", "");
+        String[] newString = cleanString.split(" ");
+        int count = 0;
+        for (int i = 0; i < newString.length; i++) {
+            if (newString[i].length() <= 3)
+                count++;
+        }
+        String[] result = new String[count];
+        int j = 0;
+        for (int i = 0; i < newString.length; i++) {
+            if (newString[i].length() <= 3) {
+                result[j] = newString[i];
+                j++;
+            }
+        }
 
+        System.out.println(Arrays.toString(newString) + " -> " + Arrays.toString(result));
+
+    }
 
 
 }
